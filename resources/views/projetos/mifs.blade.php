953324c8b79578fd98900d7a835fa8ab9246b419
Joao <juaorik@gmail.com>
2018-09-27 15:27:09 -0300

    update
    update

--- resources/views/projetos/mifs.blade.php
+++ resources/views/projetos/mifs.blade.php
@@ -20,7 +20,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>Editar Projeto: </h2>
+                        <h2>MIF's do Projeto: </h2>
                         <div class="clearfix"></div>
                     </div>
                     <div class="x_content">
